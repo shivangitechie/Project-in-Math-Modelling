92a6251ce98e438ee3c07476430413ceed6c73e9
shivangi23206952 <shivangi.dubey@ucdconnect.ie>
2024-08-14 16:29:01 +0100

    Create evaluations.py

---
+++ Evaluation/evaluations.py
@@ -0,0 +1,9 @@
+# Evaluate deep learning model
+dl_accuracy = test_accuracy
+
+# Evaluate linear regression model
+lr_mse = mse
+
+# Print model evaluations
+print(f"Deep Learning Model Accuracy: {dl_accuracy:.2f}")
+print(f"Linear Regression Model MSE: {lr_mse:.2f}")
